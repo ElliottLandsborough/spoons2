5398a0c30aa898fd32f74e047eb5430b964bdcb0
Elliott Landsborough <elliott.landsborough@codegent.com>
2015-11-08 14:13:04 +0000

    Conf

---
+++ config/spoons.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'jsonUrl' => 'http://www.jdwetherspoon.co.uk/home/ajax/layout_find_pubs',
+    'jsonDir' => base_path() . DIRECTORY_SEPARATOR . 'jsonFiles',
+    'googleApi' => env('googleApi', false)
+
+];
