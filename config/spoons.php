e9fe0a30c6fb2969038c11acefa77974218217e3
Elliott Landsborough <elliott.landsborough@gmail.com>
2015-11-08 17:42:51 +0000

    Searchly config stuff

--- config/spoons.php
+++ config/spoons.php
@@ -5,5 +5,7 @@ return [
     'jsonUrl' => 'http://www.jdwetherspoon.co.uk/home/ajax/layout_find_pubs',
     'jsonDir' => base_path() . DIRECTORY_SEPARATOR . 'jsonFiles',
     'googleApi' => env('googleApi', false),
-
+    'searchlyApi' => env('searchlyApi', false),
+    'searchlyDomain' => env('searchlyDomain', false),
+    'searchlyUrl' => 'https://site:' . env('searchlyApi', false) . '@' . rtrim(env('searchlyDomain', false),'/') . '/',
 ];
