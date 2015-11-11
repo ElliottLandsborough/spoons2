2f716feac5e815052b2916e9bb9b2bd9338dd8f2
Elliott Landsborough <elliott.landsborough@gmail.com>
2015-11-11 23:05:51 +0000

    Show angular view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 <!doctype html>
-<html ng-app="commentApp">
+<html ng-app="spoonsLocator">
   <head>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular-resource.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular-route.min.js"></script>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script type="text/javascript" src="/js/all.js"></script>
+    <base href="/">
   </head>
-  <body>
-
+  <body ng-app="spoonsLocator" ng-controller="homeController">
+  	<div ng-view></div>
   </body>
 </html>
